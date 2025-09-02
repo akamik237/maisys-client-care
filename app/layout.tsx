fc2969fb830ef18f1082426c7ce1101c64228bef
dimitri.kimaka <dimitri.kimaka@laregionalesa.lan>
2025-09-02 21:37:03 +0100

    Fix: Correct ThemeToggle import in layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import ThemeToggle from "@/components/ThemeToggle";
+import { ThemeToggle } from "@/components/ThemeToggle";
 import AppShell from "./AppShell";
 import { UserProvider } from "../context/UserContext";
 
